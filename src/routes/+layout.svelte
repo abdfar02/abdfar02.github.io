ce040eacc43278aa3cf98d697ee6f79bc01cd2d0
Abdul-Rahman Faraj-Allah <abdfar02@elevad.umea.se>
2023-01-19 12:18:27 +0100

    done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,14 @@
 
   <style>
     nav {
-      margin-left: 10%;
-      margin-right: 10%;
+      margin-left: 50%;
+      margin-right: 50%;
     }
+    .container-fluid {
+      background-size: cover;
+      background-image: url("https://media4.giphy.com/media/4jyU0IuAH6a1q/giphy.gif?cid=790b76116e6c74780283ba1f7c5af23fcd2d89c1a9e8f9d3&rid=giphy.gif&ct=g");
+
+}
   </style>
 </svelte:head>
 
